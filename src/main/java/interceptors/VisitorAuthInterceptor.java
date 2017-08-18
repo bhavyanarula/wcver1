8e1055589919c088ffb96a2cdb8285393fc396a5
bhavya <hp@192.168.0.108>
2017-08-18 22:48:38 +0530

    Signed-off-by: bhavya <hp@192.168.0.108>

--- src/main/java/interceptors/VisitorAuthInterceptor.java
+++ src/main/java/interceptors/VisitorAuthInterceptor.java
@@ -2,21 +2,23 @@ package interceptors;
 
 import java.util.Map;
 
+import org.apache.struts2.dispatcher.HttpParameters;
 import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
 
+import dao.NgoDao;
 public class VisitorAuthInterceptor  extends AbstractInterceptor {
 	private String pageOwnerCode;
         @Override
         public String intercept(ActionInvocation invocation) throws Exception {
         		System.out.println("Visitor Auth called");
                 Map<String, Object> sessionMap = invocation.getInvocationContext().getSession();
-                Map<String, Object> params = invocation.getInvocationContext().getParameters();
+                HttpParameters params = invocation.getInvocationContext().getParameters();
                 String userCode = (String)sessionMap.get("userCode");
                 String pageOwnerCode = "";
                 String result = "";
                 if(params.get("pageOwnerCode")!=null)
-                	pageOwnerCode = ((String[])params.get("pageOwnerCode"))[0];
+                	pageOwnerCode = params.get("pageOwnerCode").getValue();
                 if(pageOwnerCode.equals(userCode) || userCode==null || "".equals(userCode) || pageOwnerCode==null || "".equals(pageOwnerCode))
                 	return "IllegalAccess";
                 else{
